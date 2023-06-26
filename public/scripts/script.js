2b29cf2c5a60526b99cd4d0db1bd319a7af83a0f
brentjeh <94917751+brentjeh@users.noreply.github.com>
2023-06-26 20:55:49 +0200

    update

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -53,16 +53,6 @@ if (emptyButton) {
     });
     }
 
-<<<<<<< Updated upstream
-confirmEmptyButton.addEventListener('click', () => {
-    fetch('/empty', {
-        method: 'POST'
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log(data.message);
-        window.location.reload();
-=======
     if (confirmEmptyButton) {
         confirmEmptyButton.addEventListener('click', () => {
             fetch('/empty', {
@@ -142,7 +132,6 @@ const volgendeButtons = document.querySelectorAll('.volgende-button');
     button.addEventListener('click', function(event) {
         event.preventDefault();
         nextFieldset();
->>>>>>> Stashed changes
     });
 });
 
@@ -158,34 +147,7 @@ const rondAfButton = document.getElementById('rondaf-button');
 rondAfButton.addEventListener('click', function(event) {
   const isFormValid = validateForm(); // Functie om het formulier te valideren
 
-<<<<<<< Updated upstream
-
-const fieldset1 = getElementById('fieldset1');
-const fieldset2 = getElementById('fieldset2');
-const fieldset3 = getElementById('fieldset3');
-const fieldset4 = getElementById('fieldset4');
-const fieldset5 = getElementById('fieldset5');
-
-const fieldset1VolgendeKnop = getElementById('fieldset1-volgende-knop');
-const fieldset2TerugKnop = getElementById('fieldset2-terug-knop');
-const fieldset2VolgendeKnop = getElementById('fieldset2-volgende-knop');
-const fieldset3TerugKnop = getElementById('fieldset3-terug-knop');
-const fieldset3VolgendeKnop = getElementById('fieldset3-volgende-knop');
-const fieldset4TerugKnop = getElementById('fieldset4-terug-knop');
-const fieldset4VolgendeKnop = getElementById('fieldset4-volgende-knop');
-const fieldset5TerugKnop = getElementById('fieldset5-terug-knop');
-const fieldset5VolgendeKnop = getElementById('fieldset5-volgende-knop');
-
-const fieldsetStap1 = getElementById('fieldset-stap1');
-const fieldsetStap2 = getElementById('fieldset-stap2');
-const fieldsetStap3 = getElementById('fieldset-stap3');
-const fieldsetStap4 = getElementById('fieldset-stap4');
-const fieldsetStap5 = getElementById('fieldset-stap5');
-
-fieldset1VolgendeKnop.addEventListener('click', function(event) {
-=======
   if (!isFormValid) {
->>>>>>> Stashed changes
     event.preventDefault();
     return; // Stop de functie als het formulier ongeldig is
   }
